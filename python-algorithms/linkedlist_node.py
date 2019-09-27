049156d66ba01987356192691a1eae4e6d745916
Spes <bloomspes@gmail.com>
2019-09-27 20:26:10 +0900

    단일 연결리스트 구현

---
+++ python-algorithms/linkedlist_node.py
@@ -0,0 +1,30 @@
+# 단일 연결 리스트를 Node와 pointer로 구현
+
+class Node(object):
+    def __init__(self, value=None, pointer=None):
+        self.value = value
+        self.pointer = pointer
+
+    def getData(self):
+        return self.value
+
+    def getNext(self):
+        return self.pointer
+
+    def setData(self, newdata):
+        self.value = newdata
+
+    def setNext(self, newpointer):
+        self.pointer = newpointer
+
+
+if __name__ == "__main__":
+    L = Node("a", Node("b", Node("c", Node("d"))))
+    assert(L.pointer.pointer.value == "c")
+
+    print(L.getData())
+    print(L.getNext().getData())
+    L.setData("aa")
+    L.setNext(Node("e"))
+    print(L.getData())
+    print(L.getNext().getData())
