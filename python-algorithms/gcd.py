8807fbbb80dd6df6565656494c889e8ea80a95a3
Spes <bloomspes@gmail.com>
2019-10-26 21:11:36 +0900

    최대공약수 예제

---
+++ python-algorithms/gcd.py
@@ -0,0 +1,12 @@
+def gcd(a, b):
+    # b가 0이 될때까지 반복 작업 수행
+    if b == 0:
+        return b
+    while b:
+        # b를 a에 대입
+        # a를 b로 나눈 나머지를 y에 대입
+        a, b = b, a%b
+    return a
+
+
+print(gcd(357, 234))
